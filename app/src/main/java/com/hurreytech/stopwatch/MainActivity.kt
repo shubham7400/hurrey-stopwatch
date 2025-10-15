4157742fd089221be7754fea61348b287d39ba9e
learningproject740-admin <shubhammogarkar740@gmail.com>
2025-10-15 16:49:01 +0530

    Stopwatch UI layout with buttons and lap list

--- app/src/main/java/com/hurreytech/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hurreytech/stopwatch/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.hurreytech.stopwatch.ui.StopwatchScreen
 import com.hurreytech.stopwatch.ui.theme.StopwatchTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StopwatchTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                StopwatchScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StopwatchTheme {
-        Greeting("Android")
     }
 }
